f6fd3070915bf105a07f9865e44335676104754c
vilmar-junior <41882583+vilmar-junior@users.noreply.github.com>
2020-08-10 21:16:52 -0300

    Update Executavel.java

--- src/main/java/executavel/Executavel.java
+++ src/main/java/executavel/Executavel.java
@@ -3,7 +3,7 @@ package executavel;
 import view.Menu;
 
 public class Executavel {
-
+	//Alteração feita via editor no GitHub
 	public static void main(String[] args) {
 		
 		Menu menu = new Menu();

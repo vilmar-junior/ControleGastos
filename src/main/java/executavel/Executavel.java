fd9b62ec006dd6f82a41128530cc03759186bded
vcpjr <vilmarcesarpereira@gmail.com>
2020-08-12 19:31:38 -0300

    Novo commit no executável.

--- src/main/java/executavel/Executavel.java
+++ src/main/java/executavel/Executavel.java
@@ -3,12 +3,9 @@ package executavel;
 import view.Menu;
 
 public class Executavel {
-	//Alteração feita via editor no GitHub
 	public static void main(String[] args) {
-		//Alterei localmente
-		Menu menu = new Menu();
-		menu.apresentarMenu();
-
+		Menu menuDaAplicacao = new Menu();
+		menuDaAplicacao.apresentarMenu();
 	}
 
 }
